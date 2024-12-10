cf6441bf25290c6c86a31890a48d7f57fc5841c4
Mark Sweitzer <msweitzer1987@gmail.com>
2024-12-10 15:40:02 -0500

    gsap header

--- frontend/src/app/components/Header.jsx
+++ frontend/src/app/components/Header.jsx
@@ -1,22 +1,46 @@
 "use client"
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import {Link} from "@nextui-org/link";
+import gsap from "gsap";
 
 import ContactForm from './ContactForm';
 
 const Header = () => {
     const [headerOpen, setHeaderOpen ] = useState(false);
     const [hideForm, setHideForm] = useState(true);
-    const menuHandler = () => {
+    const tl =  useRef(null);
+
+
+    const menuHandler = (e) => {
         setHeaderOpen(!headerOpen);
+
     }
     const formDisplayHandler = () => {
+  
         setHideForm(!hideForm);
     }
+    
+    useEffect(()=>{
+        tl.current = gsap.timeline({paused: true})
+        tl.current.to('.ms-mega-menu', {opacity: 1, duration: 1, top: 0});
+        tl.current.to('.header-link', {opacity: 1, duration: 1, stagger: 0.3, marginTop: 0}, ">-0.7");
+    },[]);
+
+    useEffect(()=>{
+        if(headerOpen) {
+            tl.current.play();
+        } else {
+            tl.current.timeScale(3);
+            tl.current.reverse();
+   
+
+        }
+    },[headerOpen])
+
 
     return(
         <>
-            <header className={`ms-mega-menu ${headerOpen ? 'ms-mega-menu-open' : ''}`}>
+            <header className={`ms-mega-menu`}>
             <div>
                 <div className="ms-container">
                     <div className="ms-flex-container">
