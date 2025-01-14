bc94f2b47b81c10d1af071f0cd46b4bfd4bc11a4
Mark Sweitzer <msweitzer1987@gmail.com>
2025-01-14 15:59:00 -0500

    contact form validation and fixes

--- frontend/src/app/api/send/route.ts
+++ frontend/src/app/api/send/route.ts
@@ -14,40 +14,31 @@ type Payload = {
 export  async function POST(req: NextRequest) {
 
 
-  try {
-    const requestData : Payload = await req.json();
-    console.log(requestData);
-    await resend.emails.send({
-      from: `${process.env.NEXT_RESEND_FROM}`,
-      to: [`${process.env.NEXT_RESEND_TO}`],
-      subject: 'Email from MarkSweitzer.io',
-      react: EmailTemplate({ 
-        firstName: requestData.firstName, 
-        lastName: requestData.lastName, 
-        email: requestData.email, 
-        message: requestData.message
-      }),
-    });
+  const requestData : Payload = await req.json();
+  console.log(requestData);
+  const { error } = await resend.emails.send({
+    from: `${process.env.NEXT_RESEND_FROM}`,
+    to: [`${process.env.NEXT_RESEND_TO}`],
+    subject: 'Email from MarkSweitzer.io',
+    react: EmailTemplate({ 
+      firstName: requestData.firstName, 
+      lastName: requestData.lastName, 
+      email: requestData.email, 
+      message: requestData.message
+    }),
+  });
+  if(error){
+    return NextResponse.json({
+      status: 500,
+      message: error.message
+    })
+  } else {
     return NextResponse.json({
       status: 200,
       message: "Email was successfully sent."
     })
   }
-  catch(error : any){
-
-    return NextResponse.json({
-      status: 500,
-      message: error.message
-    })
-  }
-  finally{
-    console.log("Email endpoint was hit.")
-  }
 
 
-  // if (error) {
-  //   return console.error({ error });
-  // }    
-  
 
 };
