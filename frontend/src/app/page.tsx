6ab14679c17316ae3515d52aa36e423a57f65686
Mark Sweitzer <msweitzer1987@gmail.com>
2025-01-31 23:55:55 -0500

    initial commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,24 @@
 
 import HomeContent from './components/HomeContent';
 import { Metadata } from 'next';
+import { client } from "./sanity/client";
+import { type SanityDocument } from "next-sanity";
 
 export const metadata: Metadata = {
   title: 'Mark Sweitzer | Home',
   description:
     'Crafting exceptional web experiences through innovation and expertise.',
 };
-export default function Home() {
+
+const POSTS_QUERY = `*[_type == "post"]{ _id, title }`;
+
+const options = { next: { revalidate: 30 } };
+
+export default async function Home() {
+  const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
+  console.log("Posts")
+  console.log(posts)
+
   return (
     <>
       <HomeContent/>
