4428c947c47a595407e039b64ff9f6d0c0a1edf3
Mark Sweitzer <msweitzer1987@gmail.com>
2025-02-04 14:50:36 -0500

    Removed comments from profile content

--- frontend/src/app/components/ProfileContent.jsx
+++ frontend/src/app/components/ProfileContent.jsx
@@ -60,14 +60,7 @@ const ProfileContent = (props) => {
     gsap.registerPlugin(useGSAP);
     const { jobTitle } = useMsContext();
 
-    // useEffect(()=>{
-    //     const filteredJob = getJob(jobTitle);
-    //     console.log("Job Title - Use Effect: ", `${jobTitle}` )
-    //     console.log("Filtered Job")
-    //     console.log(filteredJob)
-    //     setJob(filteredJob);
-    // },[jobTitle]);
-
+ 
     const getActiveContent = (stepNumber) => {
         let output = profileState.activeContent;
         switch(stepNumber){
@@ -159,84 +152,7 @@ const ProfileContent = (props) => {
         
     }, { dependencies: [loaded], scope: container} );
 
-    // useEffect(()=>{
-    
-    // }, [profileState.activeContent])
-
-    // useGSAP(()=>{
-    
-    //     const offsets = {
-    //         offset1: -150,
-    //         offset2: -100,
-    //         offset3: -200,
-    //         offset4: -250,
-    //         profileContent: -25,
-    //         profileCallout: -75,
-    //         calloutOffset: -50
-    //     }
-    //     const tl1 = gsap.timeline({
-    //         scrollTrigger: {
-    //             trigger: '#profile-content-1',
-    //             start: '-10% center',
-    //             end: '50px center',
-    //             scrub: true,
-    //             markers: false
-    //         }
-    //     })
-
-
-    //     // tl1.to('#profile-callout-2', {opacity: .05, y : 175, filter: 'blur(0px)', duration: 2})
-    //     // tl1.to('#profile-callout-3', {opacity: .05, y : 175, filter: 'blur(0px)', duration: 3})
-    //     tl1.to('#top-title', {letterSpacing: '1rem', opacity: 0, filter: "blur(2.2rem)", y: 50});
-    //     tl1.to('#profile-callout-1', {top : offsets.profileCallout, opacity: 1});
-    //     tl1.to('#profile-content-1', {y: offsets.profileContent, opacity: 1})
- 
-    //     const tl2 = gsap.timeline({
-    //         scrollTrigger: {
-    //             trigger: '#profile-content-2',
-    //             start: '-50% center',
-    //             end: '10% center',
-    //             scrub: true,
-    //             markers: false
-    //         }
-    //     });
-    //     // tl2.to('#profile-callout-4', {opacity: .03, y : 520, filter: 'blur(0px)', duration: 6})
-    //     // tl2.to('#profile-callout-5', {opacity: .02, y : 600, filter: 'blur(0px)', duration: 1})
- 
-
-    //     tl2.to('#profile-callout-2', {top: offsets.profileCallout, opacity: 1});
-    //     tl2.to('#profile-content-2', {y: offsets.profileContent, opacity: 1})
-
-    //     // tl2.to('#profile-callout-6', {opacity: .02, y : 675, filter: 'blur(0px)', duration: 5})
-    //     const tl3 = gsap.timeline({
-    //         scrollTrigger: {
-    //             trigger: '#profile-content-3',
-    //             start: '-100% center',
-    //             end: '-50% center',
-    //             scrub: true,
-    //             markers: false
-    //         }
-    //     });
-    //     // tl3.to('#profile-callout-7', {opacity: 1, y : 1000, filter: 'blur(0px)', duration: 3})
-    //     tl3.to('#profile-callout-3', {top: offsets.profileCallout, opacity: 1});
-    //     tl3.to('#profile-content-3', {y: offsets.profileContent, opacity: 1});
-
-    //     const tl4 = gsap.timeline({
-    //         scrollTrigger: {
-    //             trigger: '#profile-content-4',
-    //             start: '-300% center',
-    //             end: '-250% center',
-    //             scrub: true,
-    //             markers: false
-    //         }
-    //     });
-    //     // tl3.to('#profile-callout-7', {opacity: 1, y : 1000, filter: 'blur(0px)', duration: 3})
-    //     tl4.to('#profile-callout-4', {top: offsets.profileCallout, opacity: 1});
-    //     tl4.to('#profile-content-4', {y: offsets.profileContent, opacity: 1})
-
-
-
-    // });
+   
 
     return(
         <div className="profile" ref={container}  >
