23e3deabb9b23023bce0b57dc9db5f3d7f951104
Mark Sweitzer <msweitzer1987@gmail.com>
2025-02-05 14:09:30 -0500

    portfolio meta information added / module created

---
+++ frontend/src/app/components/Portfolio.jsx
@@ -0,0 +1,129 @@
+"use client"
+
+import Header from './Header';
+import {Image} from "@nextui-org/react";
+import {  useState } from 'react';
+import NextUIModal from './NextUIModal';
+import {portfolioData} from '../data/portfolio';
+import Footer from './Footer';
+
+ const Portfolio = () => {
+    const [isBlurred, setIsBlurred ] = useState(false);
+    const [portfolioItem, setPortfolioItem] = useState({ name: '', description: '', image: '', title: ''});
+  
+
+    const selectPortfolioItem = (portfolioItem) => {
+        const output = portfolioData.filter(cur => cur.name === portfolioItem)[0];
+        return output;
+    }
+
+    const clickHandler = (portfolioItem) => {
+        if(portfolioItem != null){
+            const activePortfolioObj = selectPortfolioItem(portfolioItem);
+            setPortfolioItem(activePortfolioObj);
+
+        } 
+        setIsBlurred(!isBlurred);
+    }
+    return (
+        <>        
+            <Header/>
+            <div className="page">
+                <div className="portfolio-heading">
+                        <h2>Portfolio</h2>
+                </div>
+                <div className="portfolio">
+       
+                    <div className="ms-flex-container">
+                        <div className="portfolio-layout">
+                            <div className="instagrid">
+                                <div className={`portfolio-item item-1 ${isBlurred ? 'portfolio-item-blurred' : ''}`}>
+                                    <Image
+                                        alt="NextUI Fruit Image with Zoom"
+                                        src="/portfolio/item-3.webp"
+                                        onClick={()=>clickHandler('item1')}
+                                        />
+                            
+                                </div>
+                                <div className="portfolio-item item-2">
+                                    <Image
+                                        alt="NextUI Fruit Image with Zoom"
+                                        src="/portfolio/item-2.webp"
+                                        onClick={()=>clickHandler('item2')}
+                                        />
+                                </div>
+                                <div className="portfolio-item item-3">
+                                    <Image
+                                        alt="NextUI Fruit Image with Zoom"
+                                        src="/portfolio/item-9.webp"
+                                        onClick={()=>clickHandler('item3')}
+                                        />
+                                </div>
+                            </div>
+                            <div className="instagrid">
+                                <div className="portfolio-item item-1">
+                                    <Image
+                                        alt="NextUI Fruit Image with Zoom"
+                                        src="/portfolio/item-11.webp"
+                                        onClick={()=>clickHandler('item4')}
+                                        />
+                                </div>
+                                <div className="portfolio-item item-2">
+                                    <Image
+                                        alt="NextUI Fruit Image with Zoom"
+                                        src="/portfolio/item-1.webp"
+                                        onClick={()=>clickHandler('item5')}
+                                    />
+                                </div>
+                                <div className="portfolio-item item-3">
+                                    <Image
+                                    alt="NextUI Fruit Image with Zoom"
+                                        src="/portfolio/item-4.webp"
+                                        onClick={()=>clickHandler('item6')}
+                                    />
+                                </div>
+                            </div>
+                            <div className="instagrid">
+                                <div className="portfolio-item item-1">
+                                        <Image
+                                        alt="NextUI Fruit Image with Zoom"
+                                        src="/portfolio/item-5.webp"
+                                        onClick={()=>clickHandler('item7')}
+                                        />
+                                </div>
+                                <div className="portfolio-item item-2">
+                                        <Image
+                                        alt="NextUI Fruit Image with Zoom"
+                                        src="/portfolio/item-6.webp"
+                                        onClick={()=>clickHandler('item8')}
+                                        />
+                                </div>
+                                <div className="portfolio-item item-3">
+                                        <Image
+                                        alt="NextUI Fruit Image with Zoom"
+                                        src="/portfolio/item-10.webp"
+                                        onClick={()=>clickHandler('item9')}
+                                        />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <NextUIModal
+                    isOpen={isBlurred}
+                    toggleModal={clickHandler}
+                    title={`${portfolioItem.title}`}
+                    description={`${portfolioItem.description}`}
+                    ba
+                    >
+                        <Image
+                        alt="NextUI Fruit Image with Zoom"
+                        src={`/portfolio/${portfolioItem.image}`}
+                        />
+                </NextUIModal>
+            </div>
+            <Footer/>
+       </>
+    ); 
+}
+export default Portfolio;
