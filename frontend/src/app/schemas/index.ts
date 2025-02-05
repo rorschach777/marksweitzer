f1ab7c6422961c99bc4e117d13e371856e07551b
Mark Sweitzer <msweitzer1987@gmail.com>
2025-02-04 21:39:14 -0500

    portfolioType added to cms

--- frontend/src/app/schemas/index.ts
+++ frontend/src/app/schemas/index.ts
@@ -3,11 +3,13 @@ import {resumeType} from './resumeType';
 import {jobTitleType} from './jobTitleType';
 import {profileType} from './profileType';
 import {profileSectionType} from './profileSectionType';
+import {portfolioType} from './portfolioType';
 
 export const schemaTypes = [ 
     experience, 
     resumeType,
     jobTitleType, 
     profileType, 
-    profileSectionType
+    profileSectionType,
+    portfolioType
 ];
