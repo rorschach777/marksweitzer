c59eecb392301b6636c84fee63f936c5432c5f52
Mark Sweitzer <msweitzer1987@gmail.com>
2025-02-04 21:39:14 -0500

    portfolioType added to cms

---
+++ frontend/src/app/schemas/portfolioType.ts
@@ -0,0 +1,36 @@
+import {defineField, defineType} from 'sanity'
+
+export const portfolioType = defineType({
+  name: 'portfolioItemType',
+  title: 'Portfolio Item',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      type: 'string',
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'title'},
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'datetime',
+      initialValue: () => new Date().toISOString(),
+      validation: (rule) => rule.required(),
+    }),
+    defineField({
+        name: 'description',
+        type: 'string',
+        validation: (rule) => rule.required(),
+    }),
+    defineField({
+        name: 'image',
+        type: 'image',
+        validation: (rule) => rule.required()
+    })
+  ]
+})
