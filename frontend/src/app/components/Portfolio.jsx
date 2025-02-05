0a84e6c244b1d6b62c90813b40a03af15ca23c72
Mark Sweitzer <msweitzer1987@gmail.com>
2025-02-05 16:02:11 -0500

    porfolio component removed commented code

--- frontend/src/app/components/Portfolio.jsx
+++ frontend/src/app/components/Portfolio.jsx
@@ -71,77 +71,12 @@ import Footer from './Footer';
                     <div className="ms-flex-container">
                         <div className="portfolio-layout">
                             <div className="instagrid">
-                                {/* <div className={`portfolio-item item-1 ${isBlurred ? 'portfolio-item-blurred' : ''}`}>
-                                    <Image
-                                        alt="NextUI Fruit Image with Zoom"
-                                        src="/portfolio/item-3.webp"
-                                        onClick={()=>clickHandler('item1')}
-                                        />
-                            
-                                </div>
-                                <div className="portfolio-item item-2">
-                                    <Image
-                                        alt="NextUI Fruit Image with Zoom"
-                                        src="/portfolio/item-2.webp"
-                                        onClick={()=>clickHandler('item2')}
-                                        />
-                                </div>
-                                <div className="portfolio-item item-3">
-                                    <Image
-                                        alt="NextUI Fruit Image with Zoom"
-                                        src="/portfolio/item-9.webp"
-                                        onClick={()=>clickHandler('item3')}
-                                        />
-                                </div> */}
                                 {createPortfolioItems(0,3)}
                             </div>
                             <div className="instagrid">
-                                {/* <div className="portfolio-item item-1">
-                                    <Image
-                                        alt="NextUI Fruit Image with Zoom"
-                                        src="/portfolio/item-11.webp"
-                                        onClick={()=>clickHandler('item4')}
-                                        />
-                                </div>
-                                <div className="portfolio-item item-2">
-                                    <Image
-                                        alt="NextUI Fruit Image with Zoom"
-                                        src="/portfolio/item-1.webp"
-                                        onClick={()=>clickHandler('item5')}
-                                    />
-                                </div>
-                                <div className="portfolio-item item-3">
-                                    <Image
-                                    alt="NextUI Fruit Image with Zoom"
-                                        src="/portfolio/item-4.webp"
-                                        onClick={()=>clickHandler('item6')}
-                                    />
-                                </div> */}
                                 {createPortfolioItems(3,6)}
                             </div>
                             <div className="instagrid">
-                        
-                                {/* <div className="portfolio-item item-1">
-                                        <Image
-                                        alt="NextUI Fruit Image with Zoom"
-                                        src="/portfolio/item-5.webp"
-                                        onClick={()=>clickHandler('item7')}
-                                        />
-                                </div>
-                                <div className="portfolio-item item-2">
-                                        <Image
-                                        alt="NextUI Fruit Image with Zoom"
-                                        src="/portfolio/item-6.webp"
-                                        onClick={()=>clickHandler('item8')}
-                                        />
-                                </div>
-                                <div className="portfolio-item item-3">
-                                        <Image
-                                        alt="NextUI Fruit Image with Zoom"
-                                        src="/portfolio/item-10.webp"
-                                        onClick={()=>clickHandler('item9')}
-                                        />
-                                </div> */}
                                 {createPortfolioItems(6,9)}
                             </div>
                         </div>
